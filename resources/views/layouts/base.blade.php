63b20f3c870051140d88af3fc7ef966c003f3ec7
Boubacar18 <dialloboubs61@gmail.com>
2022-07-02 21:10:53 +0000

    page d'accueil front done

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
    
-<title>Sidebar 09</title>
+<title>Sen Observatoire</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -10,15 +10,17 @@
 		
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/accueil.css">
 </head>
 <body>
-   <div class="wrapper d-flex align-items-stretch">
-    @include('layouts.partials.sidebar')
-   <div id="content" class="p-4 p-md-5 pt-5">
-    @include('layouts.partials.header')
-     @yield('content')
-   </div>
-   </div>   
+  <div class="wrapper d-flex align-items-stretch">
+    @include('layouts.partials.navig')
+    <div class="container">
+   <!-- <div id="content" class="p-4 p-md-5 pt-5"> -->
+      @include('layouts.partials.header')
+      @yield('content')
+    </div>
+  </div>   
    <!-- JavaScript Bundle with Popper -->
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.js"></script>
