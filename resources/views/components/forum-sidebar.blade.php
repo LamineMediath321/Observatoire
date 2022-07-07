33279c2fdb2a72c17fbe2df1c1b68e805124b6ae
Boubacar18 <dialloboubs61@gmail.com>
2022-07-07 11:52:37 +0000

    Page d'accueil Forum done

---
+++ resources/views/components/forum-sidebar.blade.php
@@ -0,0 +1,42 @@
+
+            <!-- Side bar Actualities -->
+            <div class="sideBar">
+                <div class="head-forums">
+                    <p class="actualite">Autres Forums</p>
+                    <!-- <a href="" class="voirtout"><span>voir tout</span></a> -->
+                </div>
+                <!-- one actuality -->
+                <div class="actualite-post">
+                    <div class="actualite-img">
+                        <img src="/images/img_actu4.jpg" alt="">
+                    </div>
+                    <div class="actualite-details">
+                        <p class="titre-actu">Thème</p>
+                        <a href="" class="details"><span>voir</span></a>
+                    </div>
+                </div>
+                <!-- one actuality -->
+                <div class="actualite-post">
+                    <div class="actualite-img">
+                        <img src="/images/img_actu3.jpg" alt="">
+                    </div>
+                    <div class="actualite-details">
+                        <p class="titre-actu">Thème</p>
+                        <div class="lieu-actu">
+                        </div>
+                        <a href="" class="details"><span>voir</span></a>
+                    </div>
+                </div>
+                <!-- one actuality -->
+                <div class="actualite-post">
+                    <div class="actualite-img">
+                        <img src="/images/img_actu2.jpg" alt="">
+                    </div>
+                    <div class="actualite-details">
+                        <p class="titre-actu">Thème</p>
+                        <div class="lieu-actu">
+                        </div>
+                        <a href="" class="details"><span>voir</span></a>
+                    </div>
+                </div>
+               
--- resources/views/components/topbarforum.blade.php
+++ resources/views/components/topbarforum.blade.php
@@ -4,10 +4,7 @@
   </div>
   <div class="boutonInscrip">
    
-    <!-- @guest
-        <a class="btn btn-primary" href="{{ route('register') }}" role="button">Inscription</a>
-        <a class="btn btn-outline-primary" href="{{ route('login') }}" role="button">Connexion</a>
-    @endguest -->
+    
     @auth
       <form method="POST" action="{{ route('logout') }}">
         @csrf
